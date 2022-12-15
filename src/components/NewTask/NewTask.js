942356f4b5afa8bdd9d938ad744a8ba48e8e40cb
flare-s <webflarez@gmail.com>
2022-12-15 18:16:13 +0000

    Used the fetch hook in newTask component

--- src/components/NewTask/NewTask.js
+++ src/components/NewTask/NewTask.js
@@ -1,41 +1,32 @@
-import { useState } from "react";
+import useFetch from "../../hooks/useFetch";
 
 import Section from "../UI/Section";
 import TaskForm from "./TaskForm";
 
 const NewTask = (props) => {
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const { isLoading, error, fetchRequest } = useFetch();
+
+  const addTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
 
   const enterTaskHandler = async (taskText) => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch(
-        "https://react-custom-hooks-7a2ed-default-rtdb.firebaseio.com/tasks.json",
-        {
+    fetchRequest(
+      {
+        url: "https://react-custom-hooks-7a2ed-default-rtdb.firebaseio.com/tasks.json",
+        config: {
           method: "POST",
           body: JSON.stringify({ text: taskText }),
           headers: {
             "Content-Type": "application/json",
           },
-        }
-      );
-
-      if (!response.ok) {
-        throw new Error("Request failed!");
-      }
-
-      const data = await response.json();
-
-      const generatedId = data.name; // firebase-specific => "name" contains generated id
-      const createdTask = { id: generatedId, text: taskText };
-
-      props.onAddTask(createdTask);
-    } catch (err) {
-      setError(err.message || "Something went wrong!");
-    }
-    setIsLoading(false);
+        },
+      },
+      addTask.bind(null, taskText)
+    );
   };
 
   return (
