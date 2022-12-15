fef5958cb9f6ce95290faec390a2f8d5e4a6bed6
flare-s <webflarez@gmail.com>
2022-12-15 17:07:06 +0000

    Added fetch hook logic

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,3 +1,32 @@
-const useFetch = () => {};
+import { useState } from "react";
+const useFetch = (requestConfig, handleData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchRequest = async () => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, requestConfig.config);
+
+      if (!response.ok) {
+        throw new Error("Request failed!");
+      }
+
+      const data = await response.json();
+
+      handleData(data);
+    } catch (err) {
+      setError(err.message || "Something went wrong!");
+    }
+    setIsLoading(false);
+  };
+
+  return {
+    isLoading,
+    error,
+    fetchRequest,
+  };
+};
 
 export default useFetch;
